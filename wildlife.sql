f37f9b73ca826d14457256dea48a473cf0adf10f
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-26 00:14:22 +0600

    Species data added.

--- wildlife.sql
+++ wildlife.sql
@@ -29,3 +29,12 @@ INSERT INTO rangers (name, region) VALUES
 ('Alice Green', 'Northern Hills'),
 ('Bob White', 'River Delta'),
 ('Carol King', 'Mountain Range');
+
+
+--Species data add
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+

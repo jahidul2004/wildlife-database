a9455d89b19b5bb3cebf1c52d81517bf7f3bc57e
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-25 23:57:31 +0600

    Species table added.

--- wildlife.sql
+++ wildlife.sql
@@ -1,7 +1,15 @@
+--Creating Raners Table
 CREATE TABLE rangers(
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     region VARCHAR(100) NOT NULL
 );
 
-SELECT * FROM rangers;
+--Creating Species Table
+CREATE TABLE species(
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) CHECK (conservation_status IN('Endangered', 'Vulnerable', 'Historic'));
+)

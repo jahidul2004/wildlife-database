adf908536f413c99a4a3a9ad53109276fa3844b0
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-26 01:26:14 +0600

    Problem: 9 Delete rangers who have never sighted any species solved.

--- wildlife.sql
+++ wildlife.sql
@@ -93,3 +93,7 @@ SELECT sighting_id,
         ELSE 'Evening'
     END AS time_of_day
 FROM sightings;
+
+--Problem: 9 Delete rangers who have never sighted any species
+DELETE FROM rangers
+    WHERE ranger_id NOT IN (SELECT DISTINCT ranger_id FROM sightings);

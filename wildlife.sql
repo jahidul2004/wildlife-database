1176fb0a622469679140b16dac91359c54ecdba7
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-26 00:09:07 +0600

    Sighting table added.

--- wildlife.sql
+++ wildlife.sql
@@ -11,5 +11,15 @@ CREATE TABLE species(
     common_name VARCHAR(100) NOT NULL,
     scientific_name VARCHAR(100) NOT NULL,
     discovery_date DATE NOT NULL,
-    conservation_status VARCHAR(50) CHECK (conservation_status IN('Endangered', 'Vulnerable', 'Historic'));
-)
+    conservation_status VARCHAR(50) CHECK (conservation_status IN('Endangered', 'Vulnerable', 'Historic'))
+);
+
+-- Creating Sightings
+CREATE TABLE sightings(
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers(ranger_id),
+    location VARCHAR(150) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL,
+    notes TEXT
+);
